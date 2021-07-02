e9d0b6111baed9339cc5f5b38992e28ad3c45c50
Forrest Longanecker <forrest.longanecker@macbook>
2021-07-02 08:07:21 -0700

    added first bash template to single-file-templates - basic-case.sh

---
+++ single-file-templates/basic-case.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+while getopts a:bc: flag
+do
+	case "${flag}" in
+		a) a_value=${OPTARG};;
+		b) b_value=true;;
+		c) c_value=${OPTARG};;
+	esac
+done
+
+echo "-a flag is set to $a_value"
+
+if  [ "$b_value" == "true" ]; then
+	echo "if -b flag is present we will print this"
+fi
+
+echo "-c flag is set to $c_value"
